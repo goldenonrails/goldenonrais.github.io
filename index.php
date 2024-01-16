8ae0d6d9215a259c7376d36b9335089ca3c691cb
goldenonraisl <adriansantosrollim@gmail.com>
2024-01-16 09:36:12 -0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>golden on rails | programming</title>
+</head>
+<body>
+    <h1>Golden On Rails</h1>
+    <nav>
+        <ul>
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Others WebSites</a></li>
+            <li><a href="#">Games</a></li>
+        </ul>
+    </nav>
+    <hr>
+
+    
+</body>
+</html>
